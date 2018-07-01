0c5ff889e296779bf901510584c2b2d2eb4d0878
Sorse <sorse1000@gmail.com>
2018-07-01 19:54:11 +0300

    taxi

---
+++ w-taxi/js/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+
+
+/* smooth scrolling */
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            return false;
+          } else {
+            $target.attr('tabindex','-1');
+            $target.focus();
+          };
+        });
+      }
+    }
+  });
+
+/* open menu */
+var menu = $(".open-me");
+menu.on("click", function() {
+  $(this).children().toggleClass("active-menu");
+  $(this).siblings().children().removeClass("active-menu");
+});
+
+var mob_menu = $(".open-mobile");
+mob_menu.on("click", function() {
+  $(".mobile-menu-inner").toggleClass("active-mobile-menu");
+});
+
+
+/* go top */
+$(function() {
+ $.fn.scrollToTop = function() {
+  if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
+  var scrollDiv = $(this);
+  $(window).scroll(function() {
+   if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
+   else $(scrollDiv).fadeIn("slow")
+  });
+  $(this).click(function() {
+   $("html, body").animate({scrollTop: 0}, "slow")
+  })
+ }
+});
+
+$(function() {
+ $(".to-top").scrollToTop();
+});
+});
